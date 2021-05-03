84046b6ecee9556c9d06914449fff734d0911ba8
RockTF <egorov2001egorr@gmail.com>
2021-05-03 13:25:56 +0300

    New paint

--- NewPaitnt/NewPaitnt/Program.cs
+++ NewPaitnt/NewPaitnt/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace NewPaintProject
+namespace NewPaitnt
 {
     static class Program
     {
--- NewPaitnt/NewPaitnt/obj/Debug/net5.0-windows/NewPaitnt.AssemblyInfo.cs
+++ NewPaitnt/NewPaitnt/obj/Debug/net5.0-windows/NewPaitnt.AssemblyInfo.cs
@@ -11,13 +11,15 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("NewPaintProject")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("NewPaitnt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("NewPaintProject")]
-[assembly: System.Reflection.AssemblyTitleAttribute("NewPaintProject")]
+[assembly: System.Reflection.AssemblyProductAttribute("NewPaitnt")]
+[assembly: System.Reflection.AssemblyTitleAttribute("NewPaitnt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
+[assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
 

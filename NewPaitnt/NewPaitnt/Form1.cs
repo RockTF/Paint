b508c042805b4bfcb84105fd31cee486dc7c3b28
AntonChubarov <achlp600@gmail.com>
2021-05-04 19:55:08 +0300

    Improve architecture

--- NewPaitnt/NewPaitnt/Form1.cs
+++ NewPaitnt/NewPaitnt/Form1.cs
@@ -14,45 +14,25 @@ namespace NewPaitnt
 {
     public partial class mainPaint : Form
     {
-        public static Bitmap MainImage { get; set; }
-        public static List<Bitmap> History { get; set; }
-        public static int Xclick { get; set; }
-        public static int Yclick { get; set; }
-        public static int Xstart { get; set; }
-        public static int Ystart { get; set; }
-        public static int Xend { get; set; }
-        public static int Yend { get; set; }
-
         public mainPaint()
         {
             InitializeComponent();
         }
 
-        public void CalculateCoordinates(int Xcurrent, int Ycurrent)
-        {
-            Xstart = (Xclick < Xcurrent) ? Xclick : Xcurrent;
-            Xend = (Xclick < Xcurrent) ? Xcurrent : Xclick;
-            Ystart = (Yclick < Ycurrent) ? Yclick : Ycurrent;
-            Yend = (Yclick < Ycurrent) ? Ycurrent : Yclick;
-        }
-
         private void mainPaint_Load(object sender, EventArgs e)
         {
             Settings.Initialize();
-            MainImage = new Bitmap(Settings.ImageWidth, Settings.ImageHeight);
-            DrawingEngine.ClearTransparent = new Bitmap(Settings.ImageWidth, Settings.ImageHeight);
-            DrawingEngine.MainGraphics = Graphics.FromImage(MainImage);
-            DrawingEngine.MainGraphics.Clear(Color.White);
-            pictureBoxPaint.Image = MainImage;
+            DrawingEngine.Initialize();
+            pictureBoxPaint.Image = DrawingEngine.MainImage;
         }
 
         private void pictureBoxPaint_MouseDown(object sender, MouseEventArgs e)
         {
             if (e.Button == MouseButtons.Left)
             {
-                DrawingEngine.TempImage = (Bitmap)MainImage.Clone();
-                Xclick = e.X;
-                Yclick = e.Y;
+                DrawingEngine.MainImageToTemporary();
+                DrawingEngine.Xclick = e.X;
+                DrawingEngine.Yclick = e.Y;
             }
         }
 
@@ -60,11 +40,9 @@ namespace NewPaitnt
         {
             if (e.Button == MouseButtons.Left)
             {
-                CalculateCoordinates(e.X, e.Y);
+                DrawingEngine.CalculateCoordinates(e.X, e.Y);
                 DrawingEngine.Draw();
-                pictureBoxPaint.Image = MainImage;
-                GC.Collect();
-                GC.WaitForPendingFinalizers();
+                pictureBoxPaint.Image = DrawingEngine.MainImage;
             }
         }
     }
--- NewPaitnt/NewPaitnt/Implementation/DrawingEngine.cs
+++ NewPaitnt/NewPaitnt/Implementation/DrawingEngine.cs
@@ -10,6 +10,14 @@ namespace NewPaitnt.Implementation
 {
     public static class DrawingEngine
     {
+        public static int Xclick { get; set; }
+        public static int Yclick { get; set; }
+        public static int Xstart { get; set; }
+        public static int Ystart { get; set; }
+        public static int Xend { get; set; }
+        public static int Yend { get; set; }
+        public static Bitmap MainImage { get; set; }
+        public static List<Bitmap> History { get; set; }
         public static Bitmap TempImage { get; set; }
         public static Bitmap Figure { get ; set; }
         public static Bitmap Transparent { get; set; }
@@ -17,9 +25,24 @@ namespace NewPaitnt.Implementation
         public static Graphics MainGraphics { get; set; }
         public static Graphics FigureGraphics { get; set; }
 
+        public static void Initialize()
+        {
+            MainImage = new Bitmap(Settings.ImageWidth, Settings.ImageHeight);
+            ClearTransparent = new Bitmap(Settings.ImageWidth, Settings.ImageHeight);
+            MainGraphics = Graphics.FromImage(MainImage);
+            MainGraphics.Clear(Color.White);
+        }
         public static void ClearCanvas()
         {
             MainGraphics.Clear(Color.White);
+        }
+
+        public static void CalculateCoordinates(int Xcurrent, int Ycurrent)
+        {
+            Xstart = (Xclick < Xcurrent) ? Xclick : Xcurrent;
+            Xend = (Xclick < Xcurrent) ? Xcurrent : Xclick;
+            Ystart = (Yclick < Ycurrent) ? Yclick : Ycurrent;
+            Yend = (Yclick < Ycurrent) ? Ycurrent : Yclick;
         }
 
         public static void Draw()
@@ -56,12 +79,14 @@ namespace NewPaitnt.Implementation
 
         public static void DrawRectangle()
         {
-            mainPaint.MainImage = (Bitmap)TempImage.Clone();
-            MainGraphics = Graphics.FromImage(mainPaint.MainImage);
+            MainImage = (Bitmap)TempImage.Clone();
+            MainGraphics = Graphics.FromImage(MainImage);
             Transparent = (Bitmap)ClearTransparent.Clone();
             FigureGraphics = Graphics.FromImage(Transparent);
-            FigureGraphics.DrawRectangle(Settings.Pen, mainPaint.Xstart, mainPaint.Ystart, mainPaint.Xend - mainPaint.Xstart, mainPaint.Yend - mainPaint.Ystart);
+            FigureGraphics.DrawRectangle(Settings.Pen, Xstart, Ystart, Xend - Xstart, Yend - Ystart);
             MainGraphics.DrawImage(Transparent, 0, 0);
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
         }
 
         public static void DrawSmoothCurve()
@@ -69,9 +94,24 @@ namespace NewPaitnt.Implementation
             throw new NotImplementedException();
         }
 
-        public static void Initialize()
+        public static void MainImageToTemporary()
         {
-            throw new NotImplementedException();
+            TempImage = (Bitmap)MainImage.Clone();
+        }
+
+        public static void SaveToHistory()
+        {
+
+        }
+
+        public static void Undo()
+        {
+
+        }
+
+        public static void Redo()
+        {
+
         }
     }
 }

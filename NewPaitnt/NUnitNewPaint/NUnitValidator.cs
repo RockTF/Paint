a3b6ed4cbcd0ec20f88be3e46349a044a69ded42
AntonChubarov <achlp600@gmail.com>
2021-06-17 21:47:24 +0300

    Validator by Interface

--- NewPaitnt/NUnitNewPaint/NUnitValidator.cs
+++ NewPaitnt/NUnitNewPaint/NUnitValidator.cs
@@ -1,22 +1,132 @@
-﻿using NUnit.Framework;
-using NewPaitnt;
-using Xunit;
+﻿using NewPaitnt;
+using NUnit.Framework;
+using NewPaitnt.Interfaces;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace NUnitNewPaint
 {
-    public class NUnitValidator
+ 
+    public class NUnitValid
     {
+        private IRegistrationValidator _valid;
+
+        [SetUp]
+        public void Setup()
+        {
+            _valid = new ValidatorCondition();
+        }
+
+        [TestCase("john - doe")]
+        [TestCase("john-doe@com")]
+        [TestCase("john-doe@example.c")]
+        [TestCase("john@1")]
+        [TestCase("john@-doe.com")]
+        [TestCase("")]       
+        [TestCase(" ")]       
+        [TestCase("john..doe@eaxmple.com")]
+        [TestCase(".john@example.com")]
+        [TestCase("john.@example.com")]
+        [TestCase("joh @example.com")]
+        [TestCase("john@example.c m")]
         
+        public void TestIsValidEmail(string testMaeil) 
+        {
+            bool exp = false;
+            bool res;
+            string message;
 
-        //[NUnit.Framework.Theory]
-        //[InlineData("Ksy")]
-        //public void InputValidation_CheckingTheInputOfTheFirstAndLastName(string exected, string name)
-        //{
-        //    var validator = new Validator();
+            (res, message) = _valid.EmailValidation(testMaeil);
+            Assert.AreEqual(exp, res);
+            Assert.AreEqual(true, message.Length > 1);
+        }
 
-        //    string result = validator.InputValidation(name);
+        [TestCase("john@doe.example.com")]
+        [TestCase("john.doe@example.com")]
+        [TestCase("john@example.com")]
+        [TestCase("john1245@example.com")]
+        [TestCase("JOHN1245@example.com")]
+        [TestCase("125JOHN1245@example.com")]
+        [TestCase("123mail.ru@gmail.com")]
 
-        //    result.Should().Be(expected);
-        //}
+        public void TestTwoIsValidEmail(string testMaeil)
+        {
+            char[] test = testMaeil.ToCharArray();
+            bool exp = true;
+
+            bool res = _valid.IsValidEmail(test);
+            Assert.AreEqual(exp, res);
+        }
+
+        
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase("1586")]
+        [TestCase("_john")]
+        [TestCase("j5oh4n")]
+        [TestCase("joh^nD%ou")]
+        [TestCase("john1541")]
+        [TestCase("john DoeBlackVlastilin")]
+        [TestCase("johnDoeBlackVlastilin")]
+        public void TestIsValidName(string testMaeil)
+        {
+            char[] test = testMaeil.ToCharArray();
+            bool exp = false;
+
+            bool res = _valid.IsValidName(test);
+            Assert.AreEqual(exp, res);
+        }
+
+        [TestCase("john")]
+        [TestCase("John")]
+        [TestCase("johnDou")]
+        [TestCase("JaSoN")]
+
+        public void TestTwoIsValidName(string testMaeil)
+        {
+            char[] test = testMaeil.ToCharArray();
+            bool exp = true;
+
+            bool res = _valid.IsValidName(test);
+            Assert.AreEqual(exp, res);
+        } 
+        
+        [TestCase("j5oh4n")]
+        [TestCase("john1541")]
+        [TestCase("johnDou")]
+        [TestCase("john+Dou")]
+        [TestCase("john_Dou")]
+        [TestCase("john-Dou")]
+        [TestCase("1johnDou2")]
+        
+        public void TestIsValidPassword(string testMaeil)
+        {
+            char[] test = testMaeil.ToCharArray();
+            bool exp = true;
+
+            bool res = _valid.IsValidPassword(test);
+            Assert.AreEqual(exp, res);
+        }
+        [TestCase(" ")]
+        [TestCase("")]
+        [TestCase("johnDoebigBlackVlastilinNagibator")]
+        [TestCase("1joh Dou2")]
+        [TestCase("joh%nD*ou")]
+        [TestCase("F#@kF$#k")]
+        public void TestTwoIsValidPassword(string testMaeil)
+        {
+            char[] test = testMaeil.ToCharArray();
+            bool exp = false;
+
+            bool res = _valid.IsValidPassword(test);
+            Assert.AreEqual(exp, res);
+        }
+
+
     }
 }
---
+++ NewPaitnt/NewPaitnt/ValidatorCondition.cs
@@ -0,0 +1,259 @@
+﻿using NewPaitnt.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace NewPaitnt
+{
+    public class ValidatorCondition : IRegistrationValidator
+    {
+        public (bool, string) NameValidation(string name)
+        {
+            char[] input = name.ToCharArray();
+            if (input == null)
+            {
+                return (false, "Input is null!");
+            }
+            char ch;
+            int index = 0;
+            if (input.Length < 3 || input.Length > 20)
+            {
+                return (false, "The input line must contain more than 2 letters and be no longer than 30");
+            }
+            else
+            {
+                while (index < input.Length)
+                {
+                    ch = input[index];
+
+                    if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || ch == '-')
+
+                    {
+                        index++;
+                    }
+                    else
+                    {
+                        return (false, "The input line can contain only Latin characters and cannot contain invalid characters");
+                    }
+                }
+            }
+            return (true, "");
+        }
+        public (bool, string) PasswordValidation(string password)
+        {
+            char[] input = password.ToCharArray();
+            if (input == null)
+            {
+                return (false, "Input is null!");
+            }
+            char ch;
+            int index = 0;
+            if (input.Length < 6 || input.Length > 30)
+            {
+                return (false, "The password must contain more than 5 letters and be no longer than 30");
+            }
+            else
+            {
+                while (index < input.Length)
+                {
+                        ch = input[index];
+                    if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')
+                                                || (ch >= '0' && ch <= '9') || ch == '_' || ch == '-'
+                                                || ch == '+')
+
+                    {
+                        index++;
+                    }
+                    else
+                    {
+                        return (false, "The password must contain only Latin characters and cannot contain invalid characters");
+                    }
+                }
+            }
+            return (true, "");
+        }
+        public (bool, string) EmailValidation(string email)
+        {
+            char[] input = email.ToCharArray();
+            if (input == null)
+            {
+                return (false, "Input is null!");
+            }
+
+            int state = 0;
+            char ch;
+            int index = 0;
+            int mark = 0;
+            String local = null;
+            List<String> domain = new List<String>();
+
+            while (index <= input.Length && state != -1)
+            {
+
+                if (index == input.Length)
+                {
+                    ch = '\0';
+                }
+                else
+                {
+                    ch = input[index];
+                    if (ch == '\0')
+                    {
+                        return (false, "Invalid input!");
+                    }
+                }
+
+                switch (state)
+                {
+
+                    case 0:
+                        {
+                            if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')
+                                    || (ch >= '0' && ch <= '9') || ch == '_' || ch == '-'
+                                    || ch == '+')
+                            {
+                                state = 1;
+                                break;
+                            }
+                            state = -1;
+                            break;
+                        }
+
+                    case 1:
+                        {
+                            if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')
+                                    || (ch >= '0' && ch <= '9') || ch == '_' || ch == '-'
+                                    || ch == '+')
+                            {
+                                break;
+                            }
+                            if (ch == '.')
+                            {
+                                state = 2;
+                                break;
+                            }
+                            if (ch == '@')
+                            {
+                                local = new String(input, 0, index - mark);
+                                mark = index + 1;
+                                state = 3;
+                                break;
+                            }
+                            state = -1;
+                            break;
+                        }
+
+                    case 2:
+                        {
+                            if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')
+                                    || (ch >= '0' && ch <= '9') || ch == '_' || ch == '-'
+                                    || ch == '+')
+                            {
+                                state = 1;
+                                break;
+                            }
+                            state = -1;
+                            break;
+                        }
+
+                    case 3:
+                        {
+                            if ((ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9')
+                                    || (ch >= 'A' && ch <= 'Z'))
+                            {
+                                state = 4;
+                                break;
+                            }
+                            state = -1;
+                            break;
+                        }
+
+                    case 4:
+                        {
+                            if ((ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9')
+                                    || (ch >= 'A' && ch <= 'Z'))
+                            {
+                                break;
+                            }
+                            if (ch == '-')
+                            {
+                                state = 5;
+                                break;
+                            }
+                            if (ch == '.')
+                            {
+                                domain.Add(new String(input, mark, index - mark));
+                                mark = index + 1;
+                                state = 5;
+                                break;
+                            }
+                            if (ch == '\0')
+                            {
+                                domain.Add(new String(input, mark, index - mark));
+                                state = 6;
+                                break;
+                            }
+                            state = -1;
+                            break;
+                        }
+
+                    case 5:
+                        {
+                            if ((ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9')
+                                    || (ch >= 'A' && ch <= 'Z'))
+                            {
+                                state = 4;
+                                break;
+                            }
+                            if (ch == '-')
+                            {
+                                break;
+                            }
+                            state = -1;
+                            break;
+                        }
+
+                    case 6:
+                        {
+                            break;
+                        }
+                }
+                index++;
+            }
+            if (state != 6)
+            {
+                return (false, "Email is not valid!");
+            }
+
+            if (domain.Count < 2)
+            {
+                return (false, "Email`s domain is not valid!");
+            }
+
+            if (local.Length > 64)
+            {
+                return (false, "Email is too long!");
+            }
+
+            if (input.Length > 254)
+            {
+                return (false, "Email is too long!");
+            }
+
+            index = input.Length - 1;
+            while (index > 0)
+            {
+                ch = input[index];
+                if (ch == '.' && input.Length - index > 2)
+                {
+                    return (true, "");
+                }
+                if (!((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')))
+                {
+                    return (false, "The input line can contain only Latin characters and cannot contain invalid characters");
+                }
+                index--;
+            }
+            return (true, "");
+        }
+    }
+}

0ec8b0d7c87a93c4630f07a8972ca3e1eef81d35
RockTF <egorov2001egorr@gmail.com>
2021-06-24 21:08:20 +0300

    Add new architecture

--- NewPaitnt/PaintServer/Controllers/RegistrationController.cs
+++ NewPaitnt/PaintServer/Controllers/RegistrationController.cs
@@ -1,11 +1,36 @@
-﻿using System;
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using PaintServer.DTO;
+using PaintServer.Server.Realization;
+using PaintServer.ServerRequest;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
 namespace PaintServer.Controllers
 {
-    public class RegistrationController
+    [Route("api/registr")]
+    [ApiController]
+    public class RegistrationController : ControllerBase
     {
+        [HttpPost]
+        //[Route("registr")]
+        public IActionResult Login([FromBody] UserRegistrationData userRegistration)
+        {
+            //Persons autorizationResultData = new Login().AutorizeUser(userAutorizationData);
+
+            AutorizationService autorizationService = new AutorizationService();
+
+            RegistrationDTO registration = autorizationService.register(userRegistration.Login, userRegistration.Password, userRegistration.Name, userRegistration.LastName, userRegistration.Admin); //переделать на обьект параметры метода
+
+            return Ok(registration);
+        }
     }
+ 
 }
--- NewPaitnt/PaintServer/Startup.cs
+++ NewPaitnt/PaintServer/Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using PaintServer.Server.Interfase;
+using PaintServer.Server.Realization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,7 +26,7 @@ namespace PaintServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddTransient<IAutorization, AutorizationService>();
             services.AddControllers();
         }
 

e35e9196be082f70c3c58f5f7902b2758c6077c2
AntonChubarov <achlp600@gmail.com>
2021-06-26 14:12:52 +0300

    delete unnecessary files

--- NewPaitnt/PaintServer/Controllers/LogInController.cs
+++ NewPaitnt/PaintServer/Controllers/LogInController.cs
@@ -1,15 +1,6 @@
-﻿using Dapper;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Data.SqlClient;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Mvc;
 using PaintServer.DTO;
 using PaintServer.Server.Realization;
-using PaintServer.ServerRequest;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
